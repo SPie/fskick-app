8978d5822c3320bee38c3a097a4f84c7505b88fc
SPie <d.spie@pm.me>
2020-11-10 20:07:10 +0100

    updated laravel to work correctly with expectsTable in tests

--- tests/Feature/Console/SeasonCommandsTest.php
+++ tests/Feature/Console/SeasonCommandsTest.php
@@ -71,8 +71,6 @@ final class SeasonCommandsTest extends FeatureTestCase
         $this->artisan('season:list')
             ->expectsTable(['Name', 'Active'], [[$season->getName(), $season->isActive()]])
             ->assertExitCode(0);
-
-        $this->expectedTables = [];
     }
 
     //endregion
